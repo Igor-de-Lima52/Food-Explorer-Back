7a39dc74f1912a5e431a472d70b18346973aba80
Igor <delimaigor781@gmail.com>
2024-08-02 11:00:39 -0300

    API's initial config

---
+++ src/server.js
@@ -0,0 +1,34 @@
+require("express-async-errors");
+require("dotenv/config");
+
+const cors = require("cors");
+const express = require("express");
+// const routes = require("./routes");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// migrationsRun();
+
+// app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
+
+// app.use(routes);
+
+app.use((error, req, res, next) => {
+  if(error instanceof AppError){
+    return res.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    });
+  }
+  console.error(error);
+  return res.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  })
+});
+
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
