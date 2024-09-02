3c5f604f9142d2b840b85dd89faa7d5694b4b34c
Igor <delimaigor781@gmail.com>
2024-09-02 11:15:48 -0300

    Updated

--- src/server.js
+++ src/server.js
@@ -9,9 +9,9 @@ const routes = require("./routes");
 const cookieParser = require("cookie-parser");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
